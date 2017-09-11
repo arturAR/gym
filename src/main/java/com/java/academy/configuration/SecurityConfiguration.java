a2dbbe8c357ed4d58f135e5baee07eb508abe5ac
Michal <michal_partacz@epam.com>
2017-09-11 11:36:02 +0200

    Added travis file and basic security filter config

--- src/main/java/com/java/academy/configuration/SecurityConfiguration.java
+++ src/main/java/com/java/academy/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.java.academy.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/**").access("hasRole('ROLE_USER')")
+                .and().formLogin().loginPage("/login")
+                .loginProcessingUrl("/login").usernameParameter("ssoId").passwordParameter("password").and()
+                .rememberMe().rememberMeParameter("remember-me").tokenRepository(null)
+                .tokenValiditySeconds(86400).and().csrf().and().exceptionHandling().accessDeniedPage("/Access_Denied");
+    }
 }
