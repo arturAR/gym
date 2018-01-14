f8b02aad8bec262ecedc3240190d420da52042ad
arturAR <artur.rosa1992@gmail.com>
2018-01-14 14:11:40 +0100

    Error page with routing

--- client-web/web-app/src/components/App.tsx
+++ client-web/web-app/src/components/App.tsx
@@ -4,6 +4,11 @@ import './App.css';
 const logo = require('./logo.svg');
 
 class App extends React.Component {
+
+    returnString(text : string) {
+        return text;
+    }
+
   render() {
     return (
       <div className="App">
@@ -14,6 +19,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+          <span>{this.returnString('Hello World!')}</span>
       </div>
     );
   }
--- client-web/web-app/src/index.tsx
+++ client-web/web-app/src/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import {Whoops404} from './components/error/Whoops404';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route, hashHistory} from 'react-router'
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+      <Route path="*" component={Whoops404}/>
+  </Router>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
