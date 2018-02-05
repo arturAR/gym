946c7f34595fc9da755027d795846c31c7161fa5
Michal <azet89@gmail.com>
2018-02-05 21:46:15 +0100

    Added getters and setters and other necessary stuff to user profile's model;

--- backend/src/main/java/com/java/academy/gym/model/UserProfile.java
+++ backend/src/main/java/com/java/academy/gym/model/UserProfile.java
@@ -3,10 +3,14 @@ package com.java.academy.gym.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "USER_PROFILES")
 public class UserProfile extends BaseEntity {
+    private static final long serialVersionUID = 7871352451627547466L;
+
+    public UserProfile() {}
 
     @Column(name = "FIRST_NAME", nullable = false)
     private String firstName;
@@ -14,8 +18,19 @@ public class UserProfile extends BaseEntity {
     @Column(name = "SECOND_NAME", nullable = false)
     private String lastName;
 
-    public UserProfile(String firstName, String lastName) {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
@@ -26,5 +41,20 @@ public class UserProfile extends BaseEntity {
                 ", lastName='" + lastName + '\'' +
                 ", id=" + id +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserProfile that = (UserProfile) o;
+        return Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(firstName, lastName);
     }
 }
