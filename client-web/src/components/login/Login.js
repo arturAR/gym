c361875bd2c08cfd416e2c27c15a77e4966151d1
Michal <azet89@gmail.com>
2018-03-24 13:26:16 +0100

    Added styles to login page.

--- client-web/src/components/login/Login.js
+++ client-web/src/components/login/Login.js
@@ -2,35 +2,45 @@ import React from 'react';
 import Button from 'material-ui/Button';
 import Card from 'material-ui/Card';
 import TextField from 'material-ui/TextField';
-import { withStyles } from 'material-ui/styles';
-import {grey} from "material-ui/es/colors/";
 
 import './Login.css';
 
-const styles = theme => ({
-    button: {
-        margin: theme.spacing.unit,
-    },
-    textfield: {
-        margin: theme.spacing.unit,
-    },
-    card: {
-        backgroundColor: grey,
-        margin: 15,
-        maxWidth: 200,
-    },
-});
+const margin = {margin: '10px'};
+
+const primaryButton = {
+    margin: '10px',
+    'background-color': 'black',
+    'color': 'white',
+    'font-weight': '700',
+};
+
+const secondaryButton = {
+    margin: '10px',
+    'background-color': 'white',
+    'color': 'black',
+    'font-weight': '700',
+};
+
+const cardStyle = {
+    'max-width': '300px',
+    margin: '25px',
+    'padding-top': '15px',
+    'padding-bottom': '15px',
+    'margin-left': 'auto',
+    'margin-right': 'auto',
+    'margin-top': '25px',
+};
 
 export default class Login extends React.Component {
 
     render() {
         return (
             <div className="login-container">
-                <Card classes={styles.card}>
-                    <TextField placeholder={"Login"} type="text" name="firstname" classes={styles.textfield}/><br/>
-                    <TextField placeholder={"Password"} type="password" name="password" classes={styles.textfield}/><br/>
-                    <Button variant="raised" color="primary" classes={styles.button}>Send</Button>
-                    <Button variant="raised" color="secondary" classes={styles.button}>Cancel</Button>
+                <Card style={cardStyle}>
+                    <TextField placeholder={"Login"} type="text" name="firstname" style={margin}/><br/>
+                    <TextField placeholder={"Password"} type="password" name="password" style={margin}/><br/>
+                    <Button variant="raised" style={primaryButton}>Send</Button>
+                    <Button variant="raised" style={secondaryButton}>Cancel</Button>
                 </Card>
             </div>
         );
